39f76e091490b79316a741c24d2b534a01302294
ghosx <ghosx@qq.com>
2019-11-29 23:39:43 +0800

    refactory: 环境变量修改

--- SignIn/task.py
+++ SignIn/task.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_path)
 
 if not os.environ.get('DJANGO_SETTINGS_MODULE'):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TiebaProject.settings')
