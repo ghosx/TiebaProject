6faaa31e95dd562c6592a0b3e56b13046468037c
ghosx <ghosx@qq.com>
2019-11-30 14:15:43 +0800

    refactory: 完善重构

--- TiebaProject/settings.py
+++ TiebaProject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "SignIn/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # SESSION
