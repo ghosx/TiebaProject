f24afaf83877bca31631459378eb80e05b52255c
ghosx <ghosx@qq.com>
2019-11-30 15:14:59 +0800

    refactory: fix something

--- TiebaProject/settings.py
+++ TiebaProject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tiebaproject',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': 'sign.heeeepin.com',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
