09e9b2db7d1a9f5dc231f614bc2abbeacb7c9f03
ghosx <ghosx@qq.com>
2019-12-01 11:02:56 +0800

    refactory: 修改admin后台显示样式

--- SignIn/admin.py
+++ SignIn/admin.py
@@ -3,18 +3,18 @@ from .models import User, Sign, SignLog
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'token', '共关注', '已签到', '未签到', '是否有效用户', 'created_time')
-    search_fields = ('username',)
+    list_display = ('id', 'username', 'token', '共关注', '已签到', '未签到', '是否有效用户', 'created_time')
+    search_fields = ('id', 'username',)
 
 
 class SignAdmin(admin.ModelAdmin):
-    list_display = ('name', 'fid', 'is_sign', 'status', 'retry_times', 'user')
-    search_fields = ('name', 'fid', 'status', 'user')
+    list_display = ('id', 'name', 'fid', 'is_sign', 'status', 'retry_times', 'user')
+    search_fields = ('id', 'name', 'fid', 'status', 'user')
 
 
 class SignLogAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_time', 'ret_log', 'user')
-    search_fields = ('name', 'user')
+    list_display = ('id', 'name', 'created_time', 'ret_log', 'user')
+    search_fields = ('id', 'name', 'user')
 
 
 admin.site.register(User, UserAdmin)
--- SignIn/apps.py
+++ SignIn/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SignConfig(AppConfig):
     name = 'SignIn'
+    verbose_name = '贴吧签到'
