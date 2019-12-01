eb56a588bbc2d5c944539797c95c56248948a2fc
ghosx <ghosx@qq.com>
2019-12-01 12:38:19 +0800

    refactory: 增加任务脚本异常自动重启脚本

---
+++ restart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# 配合 crontab 即可做到task.py出错自动恢复
+# */1 * * * * /bin/bash /www/wwwroot/TiebaProject/restart.sh
+
+ps -ef |grep 'python3 task.py' |grep -v grep > /dev/null
+if [ $? -ne 0 ]
+then
+    echo 'task.py is not running...'
+    echo 'try to restart task.py'
+    cd /www/wwwroot/TiebaProject
+    nohup python3 task.py >> task.log 2>&1 &
+else
+    echo "Main is running"
+fi
