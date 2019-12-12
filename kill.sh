ab2919a8dc539597dd2b166f3f1dadb83d7228d6
ghosx <ghosx@qq.com>
2019-12-12 21:22:28 +0800

    Create kill.sh

---
+++ kill.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# author: ghosx
+# date: 2019/12/12
+# desc: this script is used to kill the process of task.py and it's threed
+ps -ef |grep 'python3 task.py' |grep -v grep > /dev/null
+if [ $? -eq 0 ]
+then
+    ps -ef |grep 'python3 task.py' |grep -v grep | awk '{printf $2}' | xargs kill -9
+    echo 'success to kill the task'
+else
+    echo 'no such process of thr task.py'
+fi
