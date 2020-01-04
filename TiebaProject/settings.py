7f2c0815e6cbfbb86ee9a067bfc87fbffca4e576
ghosx <ghosx@qq.com>
2020-01-04 15:37:55 +0800

    Update settings.py

--- TiebaProject/settings.py
+++ TiebaProject/settings.py
@@ -85,17 +85,6 @@ DATABASES = {
     }
 }
 
-# Cache
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/6',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
