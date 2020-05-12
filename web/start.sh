cb1e1998074298fb338b89fc5aed6079aaf4bacd
ghosx <ghosx@qq.com>
2020-05-12 14:25:13 +0800

    自动化创建超级管理员

--- web/start.sh
+++ web/start.sh
@@ -8,6 +8,11 @@ python manage.py migrate
 # 导入数据
 echo "导入数据"
 python manage.py loaddata initial_data.yaml
+# 创建超级管理员
+echo "创建超级管理员"
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@qq.com', 'admin123456')" | python manage.py shell
+echo "用户名：admin"
+echo "密码：admin123456"
 # 启动后台任务
 echo "启动supervisord任务"
 supervisord -c supervisord.conf
